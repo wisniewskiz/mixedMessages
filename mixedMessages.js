c0baf4b2ac6d9d7fca89e28fa731c0900e311252
wisniewskiz <wisniewskiz@live.com>
2020-11-28 23:54:00 -0600

    add randomly generating adjectives and texting capabilities

--- mixedMessages.js
+++ mixedMessages.js
@@ -5,23 +5,35 @@
 // The model for the random message generator is as follows:
 //     *Time Period* you can expect *adjective* news in *topic*, and *adjective* luck in *topic*. 
 
-//creating helper function to randomly generate numbers and keep code dry
-const randomNumber = numOfParams => {
-    return Math.floor(Math.random() * numOfParams);
+const fortuneTeller = () => {
+
+    //creating helper function to randomly generate numbers and keep code dry
+    const randomNumber = numOfParams => {
+        return Math.floor(Math.random() * numOfParams);
+    }
+
+
+    //randomly generating time periods (today, tomorrow, this week, next week)
+    const timePeriod = () => {
+        const generator = randomNumber(4);
+        if (generator == 0) {
+            return 'Today';
+        } else if (generator == 1) {
+            return 'Tomorrow';
+        } else if (generator == 2) {
+            return 'This week';
+        } else {
+            return 'Next week';
+        }
+    };
+
+    //randomly generating adjectives (good, bad, awesome, poor, neutral)
+    const adjective = () => {
+        const adjectives = ['good', 'bad', 'awesome', 'poor', 'neutral'];
+        return adjectives[randomNumber(adjectives.length)];
+    };
+
+    return `${timePeriod()} you can expect ${adjective()}.`
 }
 
-
-//randomly generating time periods (today, tomorrow, this week, next week)
-const timePeriod = () => {
-    const generator = randomNumber(4);
-    if (generator == 0) {
-        return 'Today';
-    } else if (generator == 1) {
-        return 'Tomorrow';
-    } else if (generator == 2) {
-        return 'This week';
-    } else {
-        return 'Next week';
-    }
-};
-
+console.log(fortuneTeller());
